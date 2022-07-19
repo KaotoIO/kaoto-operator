326b8c2f3f100e3b0ac749b6c28fe740fc8e98d3
mmelko <mmelko@redhat.com>
2022-07-19 19:44:10 +0200

    Add permissions, regenerate bundle

--- controllers/roles.go
+++ controllers/roles.go
@@ -2,12 +2,11 @@ package controllers
 
 import (
 	kaotoiov1alpha1 "github.com/kaotoIO/kaoto-operator/api/v1alpha1"
-	v1 "k8s.io/api/core/v1"
 	v12 "k8s.io/api/rbac/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func CreateRoleBinding(role *v12.Role, account *v1.ServiceAccount) *v12.RoleBinding {
+func CreateRoleBinding(role *v12.Role) *v12.RoleBinding {
 	binding := v12.RoleBinding{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      "integrator-role-binding",
@@ -15,20 +14,53 @@ func CreateRoleBinding(role *v12.Role, account *v1.ServiceAccount) *v12.RoleBind
 		},
 		RoleRef: v12.RoleRef{
 			APIGroup: "rbac.authorization.k8s.io",
-			Kind:     role.Kind,
 			Name:     role.Name,
+			Kind:     "Role",
 		},
-		Subjects: []v12.Subject{
-			{
-				Kind:      "ServiceAccount",
-				Name:      account.Name,
-				Namespace: account.Namespace,
-			},
-		},
+		Subjects: []v12.Subject{{
+			Kind:      v12.ServiceAccountKind,
+			Name:      "default",
+			Namespace: role.Namespace,
+		}},
 	}
 	return &binding
 }
 
+func CreateClusterRoleBinding(role *v12.ClusterRole, namespace string) *v12.ClusterRoleBinding {
+	binding := v12.ClusterRoleBinding{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "integrator-role-binding-cr",
+			Namespace: role.Namespace,
+		},
+		RoleRef: v12.RoleRef{
+			APIGroup: "rbac.authorization.k8s.io",
+			Name:     role.Name,
+			Kind:     "ClusterRole",
+		},
+		Subjects: []v12.Subject{{
+			Kind:      v12.ServiceAccountKind,
+			Name:      "default",
+			Namespace: namespace,
+		}},
+	}
+	return &binding
+}
+
+func CreateIntegratorClusterRole(kaoto kaotoiov1alpha1.Kaoto) *v12.ClusterRole {
+	role := &v12.ClusterRole{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "integrator-role-cr",
+			Namespace: kaoto.Namespace,
+		},
+		Rules: []v12.PolicyRule{{
+			Verbs:     []string{"create", "delete", "update", "watch", "get", "list", "patch"},
+			APIGroups: []string{"camel.apache.org"},
+			Resources: []string{"kamelets", "kameletbindings"},
+		},
+		},
+	}
+	return role
+}
 func CreateIntegratorRole(kaoto kaotoiov1alpha1.Kaoto) *v12.Role {
 	role := &v12.Role{
 		ObjectMeta: metav1.ObjectMeta{
